8ff9bdb370429153681915ff043629906869752d
Nigar Mohubaliyeva <nigarmohubaliyeva@gmail.com>
2023-04-18 00:07:36 +0300

    first commit

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const EditTodoForm = ({editTodo, task}) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        type="text"
+        className="todo-input"
+        placeholder="Update Task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
+
---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        type="text"
+        className="todo-input"
+        placeholder="What is the task today?"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
